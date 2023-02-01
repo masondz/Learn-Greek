5d8e2e8474f8f1287f337d5020179b0cd67fb728
Zach <dzacharymason@gmail.com>
2023-02-01 04:24:47 -0600

    add definate articles array

--- learn-koine/src/Word.js
+++ learn-koine/src/Word.js
@@ -1,6 +1,18 @@
 import React, { useState } from 'react';
+import { greekArticles } from './greek_text/greekArticles';
+
+const checkIfArticle = (word) => {
+    if (greekArticles.includes(word.word)) {
+        console.log("it's an article!")
+    } else {
+        console.log("it is not an article :(")
+    }
+}
 
 const Word = (word) => {
+
+    checkIfArticle(word);
+
     return (
         <p>
          {
