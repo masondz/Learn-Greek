d0999f83ee1dcbf7d41524fb1f2a539b2c4b5b7b
Zach <dzacharymason@gmail.com>
2023-02-06 06:07:39 -0600

    update Word syling

--- learn-koine/src/Word.js
+++ learn-koine/src/Word.js
@@ -1,9 +1,11 @@
+import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { randomVerse } from "./features/verseSlice";
 import { selectWordSlice } from "./features/wordSlice";
+import './Word.css';
 
-
-
+//PROBABLY need to make a state for each element
+//in the three lists :(
 
 
 const Word = () => {
@@ -13,6 +15,24 @@ const Word = () => {
   console.log(parse);
 
   const dispatch = useDispatch();
+
+  const checkCase = (e) => {
+    e.preventDefault();
+    const wordCase = parse.case;
+    const wordNumber = parse.number;
+    const wordGender = parse.gender;
+    
+    let target = e.target.innerHTML;
+    
+    if (wordCase.includes(target) || wordNumber === target || wordGender.includes(target)) {
+      e.target.className = "case-option-correct"
+    } else {
+      e.target.className = "case-option-wrong"
+    }
+  }
+
+  const [, updateState] = React.useState(); //trying to force re-render
+  const forceUpdate = React.useCallback(() => updateState({}), []);
 
   let selectedWord = "";
   let describeWord = "";
@@ -27,36 +47,33 @@ const Word = () => {
     describeWord = "Not an artcle :("
   } else if (partOfSpeech !== "article" && !word) {
     describeWord = "Select an article"
-  } else if (partOfSpeech === "article") {
-    describeWord = `${parse.case} - ${parse.number} - ${parse.gender}`
   }
 
   return (
     <div>
       <p>{selectedWord}</p>
-      <p>
-        {describeWord}
-      </p>
+      <p>{describeWord}</p>
       <div className="categories">
         <ul className="cases">
-          <li>Nominative</li>
-          <li>Genative</li>
-          <li>Dative</li>
-          <li>Accusative</li>
+          <li onClick={(e) => checkCase(e)}>nominative</li>
+          <li onClick={(e) => checkCase(e)}>genitive</li>
+          <li onClick={(e) => checkCase(e)}>dative</li>
+          <li onClick={(e) => checkCase(e)}>accusative</li>
         </ul>
         <ul className="number">
-          <li>Singular</li>
-          <li>Plural</li>
+          <li onClick={(e) => checkCase(e)}>singular</li>
+          <li onClick={(e) => checkCase(e)}>plural</li>
         </ul>
         <ul className="Gender">
-          <li>Masculine</li>
-          <li>Feminine</li>
-          <li>Neuter</li>
+          <li onClick={(e) => checkCase(e)}>masculine</li>
+          <li onClick={(e) => checkCase(e)}>feminine</li>
+          <li onClick={(e) => checkCase(e)}>neuter</li>
         </ul>
       </div>
       <button
         onClick={() => {
           dispatch(randomVerse());
+          forceUpdate();
         }}
       >
         New Verse
