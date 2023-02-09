25a9866bde9689a26e3a9826265ffd1f21836418
Zach <dzacharymason@gmail.com>
2023-02-08 23:21:09 -0600

    removed unused variable

--- learn-koine/src/Word.js
+++ learn-koine/src/Word.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { isArticle } from "./features/wordSlice";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 
 const Word = (props) => {
     const [highlight, setHighlight] = useState("")
