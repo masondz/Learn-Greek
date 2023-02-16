537ae8a4eeb49878c324164a819a877324668a55
Zach <dzacharymason@gmail.com>
2023-02-16 00:02:23 -0600

    added react-router-dom and Routerprovider

--- learn-koine/src/index.js
+++ learn-koine/src/index.js
@@ -5,13 +5,21 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './store';
 import { Provider } from 'react-redux';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Provider store={store}>
+              <App />
+            </Provider>,
+  },
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
