85b574c3f15a5cd757a7787138d0de2cf2dca144
Zach <dzacharymason@gmail.com>
2023-02-21 01:01:31 -0600

    comment out unused variables

--- learn-koine/src/greek_text/vocabularyWords.js
+++ learn-koine/src/greek_text/vocabularyWords.js
@@ -5392,116 +5392,116 @@ let vocabListRaw =`ὁ, ἡ, τό	the 	19889
 Ὡσηέ, ὁ	Hosea	1
 ὡσπερεί	like, as, as though	1` 
 
-let mostCommonGK = `ὁ, ἡ, τό
-καί
-αὐτός, -ή, -ό
-σύ
-δέ
-ἐν
-ἐγώ
-εἰμί
-λέγω
-εἰς
-οὐ
-ὅς, ἥ, ὅ
-οὗτος, αὕτη, τοῦτο
-θεός, -οῦ, ὁ
-ὅτι
-πᾶς, πᾶσα, πᾶν
-μή
-γάρ
-Ἰησοῦς
-ἐκ, ἐξ
-ἐπί
-κύριος, -ου, ὁ
-ἔχω
-πρός
-γίνομαι
-διά
-ἵνα
-ἀπό
-ἀλλά
-ἔρχομαι
-τίς, τί
-ποιέω
-ἄνθρωπος, -ου, ὁ
-τὶς, τὶ
-Χριστός, -οῦ, ὁ
-ὡς
-εἰ`
+// let mostCommonGK = `ὁ, ἡ, τό
+// καί
+// αὐτός, -ή, -ό
+// σύ
+// δέ
+// ἐν
+// ἐγώ
+// εἰμί
+// λέγω
+// εἰς
+// οὐ
+// ὅς, ἥ, ὅ
+// οὗτος, αὕτη, τοῦτο
+// θεός, -οῦ, ὁ
+// ὅτι
+// πᾶς, πᾶσα, πᾶν
+// μή
+// γάρ
+// Ἰησοῦς
+// ἐκ, ἐξ
+// ἐπί
+// κύριος, -ου, ὁ
+// ἔχω
+// πρός
+// γίνομαι
+// διά
+// ἵνα
+// ἀπό
+// ἀλλά
+// ἔρχομαι
+// τίς, τί
+// ποιέω
+// ἄνθρωπος, -ου, ὁ
+// τὶς, τὶ
+// Χριστός, -οῦ, ὁ
+// ὡς
+// εἰ`
 
-let mostCommonEN = `the 
-and, even, also
-himself, herself, itself, same; he, she, it
-you
-but, and
-in (dat)
-I
-I am
-to say, speak
-into (acc)
-not, no
-who, which
-this; he, she, it
-a god, God
-that, because, since
-all, every, all things
-not, lest
-for
-Jesus, Joshua
-out of, from (gen)
-over, on, at the time of (gen); on the basis of, at (dat); on, to, against (acc)
-a lord, the Lord
-to have, hold
-to, towards, with (acc)
-to become, be
-through (gen); on account of (acc)
-in order that, that
-from (gen)
-but, yet, except
-to come, go
-who? which? what? why?
-to do, make
-a man
-someone, something, a certain one, a certain thing, anyone, anything 
-Christ, Messiah, Anointed One
-as, that, like, how, about, now
-if`
+// let mostCommonEN = `the 
+// and, even, also
+// himself, herself, itself, same; he, she, it
+// you
+// but, and
+// in (dat)
+// I
+// I am
+// to say, speak
+// into (acc)
+// not, no
+// who, which
+// this; he, she, it
+// a god, God
+// that, because, since
+// all, every, all things
+// not, lest
+// for
+// Jesus, Joshua
+// out of, from (gen)
+// over, on, at the time of (gen); on the basis of, at (dat); on, to, against (acc)
+// a lord, the Lord
+// to have, hold
+// to, towards, with (acc)
+// to become, be
+// through (gen); on account of (acc)
+// in order that, that
+// from (gen)
+// but, yet, except
+// to come, go
+// who? which? what? why?
+// to do, make
+// a man
+// someone, something, a certain one, a certain thing, anyone, anything 
+// Christ, Messiah, Anointed One
+// as, that, like, how, about, now
+// if`
 
 
-const moreCommon = `οὖν	then, accordingly
-κατά	down from, against (gen); according to, during, throughout (acc)
-μετά	with (gen); after (acc)
-ὁράω	to see
-ἀκούω	to hear
-πολύς, πολλή, πολύ	much, many (plural)
-δίδωμι	to give
-πατήρ, πατρός, ὁ	father
-ἡμέρα, -ας, ἡ	a day
-πνεῦμα, -τος, τό	a spirit, the Spirit
-υἱός, -οῦ, ὁ	a son
-εἷς, μία, ἕν	one
-ἀδελφός, -οῦ, ὁ	brother
-ἤ	or
-ἐάν	if
-περί	concerning, about (gen); around (acc)
-λόγος, -ου, ὁ	a word, the Word
-οἶδα	to know
-ἑαυτοῦ, -ῆς, -οῦ	of himself
-λαλέω	to speak
-οὐρανός, -οῦ, ὁ	heaven
-μαθητής, -οῦ, ὁ	a disciple
-λαμβάνω	to take, receive
-γῆ, γῆς, ἡ	the earth, land`
+// const moreCommon = `οὖν	then, accordingly
+// κατά	down from, against (gen); according to, during, throughout (acc)
+// μετά	with (gen); after (acc)
+// ὁράω	to see
+// ἀκούω	to hear
+// πολύς, πολλή, πολύ	much, many (plural)
+// δίδωμι	to give
+// πατήρ, πατρός, ὁ	father
+// ἡμέρα, -ας, ἡ	a day
+// πνεῦμα, -τος, τό	a spirit, the Spirit
+// υἱός, -οῦ, ὁ	a son
+// εἷς, μία, ἕν	one
+// ἀδελφός, -οῦ, ὁ	brother
+// ἤ	or
+// ἐάν	if
+// περί	concerning, about (gen); around (acc)
+// λόγος, -ου, ὁ	a word, the Word
+// οἶδα	to know
+// ἑαυτοῦ, -ῆς, -οῦ	of himself
+// λαλέω	to speak
+// οὐρανός, -οῦ, ὁ	heaven
+// μαθητής, -οῦ, ὁ	a disciple
+// λαμβάνω	to take, receive
+// γῆ, γῆς, ἡ	the earth, land`
 
-let mostCommonObj = {}
+// let mostCommonObj = {}
 
-mostCommonGK = mostCommonGK.split('\n');
-mostCommonEN = mostCommonEN.split('\n')
+// mostCommonGK = mostCommonGK.split('\n');
+// mostCommonEN = mostCommonEN.split('\n')
 
-for (let i=0; i < mostCommonEN.length; i++) {
-    mostCommonObj[mostCommonGK[i]] = mostCommonEN[i];
-}
+// for (let i=0; i < mostCommonEN.length; i++) {
+//     mostCommonObj[mostCommonGK[i]] = mostCommonEN[i];
+// }
 
 // export {mostCommonObj};
 
@@ -5519,7 +5519,7 @@ export { vocabListObj };
 
 // console.log(vocabListObj)
 
-export {mostCommonObj};
+// export {mostCommonObj};
 
 
 // console.log(mostCommonObj)
