715d0d1305dd2a6e11d6744a8844cdceae727f7b
Zach <dzacharymason@gmail.com>
2023-03-07 01:08:58 -0600

    check parse for verb

--- learn-koine/src/Verb.js
+++ learn-koine/src/Verb.js
@@ -1,22 +1,39 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { randomWord } from "./greek_text/parseLexicon";
 import { wordUsages } from "./greek_text/greekLexiconObject";
 import VerbGrid from "./VerbGrid";
 import "./Verb.css";
+import { setWord, selectWordSlice } from "./features/wordSlice";
+import { useDispatch, useSelector } from "react-redux";
+import Menu from "./Menu";
+
+
 
 const Verb = () => {
-    const [verb, setVerb] = useState("")
+    const verb = useSelector(selectWordSlice);
+    const dispatch = useDispatch();
+
+    const menuOptions = [
+        "Present Active Indicative"
+    ]
+
+    const menuLinks = [
+        "Vocabulary",
+        "Parse Practice"
+    ]
 
     useEffect(() => {
         let newVerb = randomWord(wordUsages, ["Present","Active","Indicative"])
-        setVerb(newVerb);
-    }, [])
+        dispatch(setWord(newVerb));
+    }, [dispatch])
 
     return (
         <div className="body">
+            <Menu menuOptions={menuOptions} menuLinks={menuLinks}/>
+            <br></br>
             <div>Present Active Indicative Verbs</div>
             <h1 style={{margin: "100px"}}>{verb.word}</h1>
-            <VerbGrid verb={verb}/>
+            <VerbGrid verb={verb} dispatch={dispatch} setWord={setWord} randomWord={randomWord}/>
         </div>
     )
 }
--- learn-koine/src/VerbGrid.js
+++ learn-koine/src/VerbGrid.js
@@ -1,36 +1,56 @@
+import { wordUsages } from "./greek_text/greekLexiconObject";
 import "./Word.css"
 
-const VerbGrid = ({ verse }) => {
+const VerbGrid = ({ verb, dispatch, setWord, randomWord }) => {
+
+    const handleNext = () => {
+        let caseOptions = document.getElementsByClassName("case-option")
+        console.log(caseOptions)
+        for (let i = 0; i <caseOptions.length; i++) {
+            caseOptions[i].className = "case-option";
+        }
+        let nextVerb = randomWord(wordUsages, ["Present", "Active", "Indicative"])
+        dispatch(setWord(nextVerb))
+    }
 
     const onClick = (e) => {
-        if (verse.parse.includes(e.target.innerHTML)) {
+        if (verb.parse.includes(e.target.innerHTML)) {
             console.log("correct!")
+            e.target.className = e.target.className + " correct";
         } else {
-            console.log('wrong!')
+            console.log('wrong!');
+            e.target.className = e.target.className + " wrong";
         }
     }
     
     return (
       <div>
         <div className="cases">
-            <div className={"verb-option"}>
+            <div className={"case-option"}
+                onClick={ (e) => onClick(e)}>
                 first
             </div>
-            <div className={"verb-option"}>
+            <div className={"case-option"}
+                onClick={ (e) => onClick(e)}>
                 second
             </div>
-            <div className={"verb-option"}>
+            <div className={"case-option"}
+                onClick={ (e) => onClick(e)}>
                 third
             </div>
         </div>
         <div className="cases">
-            <div className={"verb-option"}>
+            <div className={"case-option"}
+                onClick={ (e) => onClick(e)}>
                 singular
             </div>
-            <div className={"verb-option"}>
+            <div className={"case-option"}
+                onClick={ (e) => onClick(e)}>
                 plural
             </div>
         </div>
+        <br></br>
+        <button onClick={handleNext}>Next</button>
       </div>
     );
   };
