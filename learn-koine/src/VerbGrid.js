4dea0de16c91cf3df4df874f84cf9645512c3ee9
Zach <dzacharymason@gmail.com>
2023-06-14 10:28:23 -0500

    Prepare verb component for more stuff

--- learn-koine/src/VerbGrid.js
+++ learn-koine/src/VerbGrid.js
@@ -63,6 +63,7 @@ const VerbGrid = ({ dispatch, setWord, randomWord, verbMode, reset }) => {
 
   return (
     <div>
+      <VerbStepOne onClick={onClick} />
       <div className="cases">
         <div className={"case-option"} onClick={(e) => onClick(e)}>
           first
@@ -88,4 +89,48 @@ const VerbGrid = ({ dispatch, setWord, randomWord, verbMode, reset }) => {
   );
 };
 
+const VerbStepOne = ({ onClick }) => {
+  return (
+    <div className="verb-step-one">
+      <div className={"verb-options"} onClick={(e) => onClick(e)}>
+        Verb
+      </div>
+      <div className={"verb-options"} onClick={(e) => onClick(e)}>
+        Participle
+      </div>
+      <div className={"verb-options"} onClick={(e) => onClick(e)}>
+        Infinitive
+      </div>
+    </div>
+  );
+};
+
+//Verbs: Tense Voice Mood Person Number
+// ἀγαπῶ: {
+//   parse: "V-PAI-1S｜Verb, Present, Active, Indicative, first, singular",
+//   GN: "G25",
+//   gloss: "to love",
+// }
+
+// Tenses       Voices      Moods         Number      Person    Gender
+// Present      Active      Indicative    Singular    First     Masculine
+// Aorist       Middle      Subjunctive   Plural      Second    Feminine
+// Perfect      Passive     Imperative                Third     Neuter
+// Imperfect    Deponent
+// Future
+
+//Participle: Tense, Voice, Particple, Case, Number, Gender
+// ἀγαπῶν: {
+//   parse:
+//     "V-PAP-NSM｜Verb, Present, Active, Participle, nominative, singular, masculine",
+//   GN: "G25",
+//   gloss: "to love",
+// }
+
+//Infinitive: Tense, Voice, Infinitive
+// ἀγαπᾶν: {
+//   parse: "V-PAN｜Verb, Present, Active, Infinitive",
+//   GN: "G25",
+//   gloss: "to love",
+// }
 export default VerbGrid;
