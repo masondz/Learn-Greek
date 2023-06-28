1ded14dabe3ec9f55421cfc2ff5b71646e3f002e
Zach <dzacharymason@gmail.com>
2023-06-28 01:59:53 -0500

    more dynamic verb selection

--- learn-koine/src/VerbMenuOptions.js
+++ learn-koine/src/VerbMenuOptions.js
@@ -8,11 +8,49 @@ const VerbMenuOptions = ({
 }) => {
   //menu options will be an array
 
+  // const handleNext = () => {
+  //   let caseOptions = document.getElementsByClassName("case-option");
+  //   console.log(caseOptions);
+  //   for (let i = 0; i < caseOptions.length; i++) {
+  //     caseOptions[i].className = "case-option";
+  //   }
+  //   let nextVerb = randomWord(
+  //     wordUsages,
+  //     "parse",
+  //     verbMode.toLowerCase().split(" ")
+  //   );
+  //   dispatch(setWord(nextVerb));
+  // };
+
+  const controlHighlight = (e) => {
+    const charCategoryArray = document.getElementsByClassName(characteristic);
+    console.log(charCategoryArray);
+
+    for (let i = 0; i < charCategoryArray.length; i++) {
+      charCategoryArray[i].className = charCategoryArray[i].className.replace(
+        " chosen-option",
+        ""
+      );
+      charCategoryArray[i].className = charCategoryArray[i].className.replace(
+        " chosen-all-option",
+        ""
+      );
+    }
+
+    if (e.target.className.includes("verb-menu-all-selection")) {
+      e.target.className = e.target.className + " chosen-all-option";
+    } else {
+      e.target.className = e.target.className + " chosen-option";
+    }
+  };
+
   const handleAddCharacteristic = (e) => {
     e.preventDefault();
+    controlHighlight(e);
     const charOption = e.target.innerHTML.toLowerCase();
     console.log(charOption);
     const newCharOptions = { ...verbCharacteristics };
+
     if (charOption === "all") {
       newCharOptions[characteristic] = "";
       setVerbCharacteristics(newCharOptions);
@@ -29,7 +67,7 @@ const VerbMenuOptions = ({
       {menuOptions.map((option) => {
         return (
           <div
-            className="verb-menu-selection"
+            className={`${characteristic} verb-menu-selection`}
             key={option}
             onClick={handleAddCharacteristic}
           >
@@ -38,7 +76,7 @@ const VerbMenuOptions = ({
         );
       })}
       <div
-        className="verb-menu-all-selection"
+        className={`${characteristic} verb-menu-all-selection`}
         onClick={handleAddCharacteristic}
       >
         All
