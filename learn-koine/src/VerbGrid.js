4e0a65585667a933d9364235cfb66eb1c5d884ae
Zach <dzacharymason@gmail.com>
2023-07-10 20:07:51 -0500

    fix middle/passive check

--- learn-koine/src/VerbGrid.js
+++ learn-koine/src/VerbGrid.js
@@ -110,13 +110,19 @@ const VerbGrid = ({
 
   const onClick = (e) => {
     if (e.target.innerHTML === "middle/passive") {
-      if (word.parse.includes("middle") || word.parse.includes("passive") || word.parse.includes("Passive") || word.parse.includes("Middle") ){
+      if (
+        word.parse.includes("middle") ||
+        word.parse.includes("passive") ||
+        word.parse.includes("Passive") ||
+        word.parse.includes("Middle")
+      ) {
         e.target.className = e.target.className + " correct";
         if (verbMode !== "parsing") {
           isParsed();
-        } else {
-          e.target.className = e.target.className + " wrong";
         }
+      } else {
+        console.log("not middle/passive");
+        e.target.className = e.target.className + " wrong";
       }
     } else if (word.parse.includes(e.target.innerHTML)) {
       e.target.className = e.target.className + " correct";
