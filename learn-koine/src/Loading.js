37b2e236e78c3e6d58b116b857920a0575b27914
Zach <dzacharymason@gmail.com>
2023-07-17 11:18:10 -0500

    add Loading component

---
+++ learn-koine/src/Loading.js
@@ -0,0 +1,55 @@
+import { useState, useEffect } from "react";
+
+const Loading = () => {
+  const [loadingLayout, setLoadingLayout] = useState("");
+  let currentURL = window.location.pathname;
+
+  useEffect(() => {
+    const vocabularyLayout = (
+      <div className="vocab-component">
+        <div className="vocab-container">
+          <div className="card-container">
+            <div className="card-front">Wait...</div>
+          </div>
+          <div className="card-buttons">
+            <button className="button-vocabulary">{"<"}</button>
+            <button className="button-vocabulary">Learnt</button>
+            <button className="button-vocabulary">Retry</button>
+            <button className="button-vocabulary">{">"}</button>
+          </div>
+          <div>
+            <p>...</p>
+          </div>
+        </div>
+      </div>
+    );
+
+    const parseVerseLayout = (
+      <div className="verse-sentence">
+        <div
+          style={{
+            width: "800px",
+            height: "200px",
+            backgroundColor: "#14171b",
+            margin: "auto",
+            marginTop: "200px",
+          }}
+        >
+          <h2 style={{ padding: "15px", color: "gray" }}>loading...</h2>
+        </div>
+      </div>
+    );
+
+    if (currentURL === "/vocabulary") {
+      setLoadingLayout(vocabularyLayout);
+    } else {
+      setLoadingLayout(parseVerseLayout);
+    }
+  }, [currentURL]);
+
+  console.log(currentURL);
+
+  return <div>{loadingLayout}</div>;
+};
+
+export default Loading;
--- learn-koine/src/index.js
+++ learn-koine/src/index.js
@@ -7,6 +7,7 @@ import store from "./store";
 import { Provider } from "react-redux";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ErrorPage from "./error-page";
+import Loading from "./Loading";
 // import Verse from "./Verse";
 // import Vocabulary from "./Vocabulary";
 // import Verb from "./Verb";
@@ -37,12 +38,11 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
   },
 ]);
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Suspense>
+      <Suspense fallback={<Loading />}>
         <RouterProvider router={router} />
       </Suspense>
     </Provider>
