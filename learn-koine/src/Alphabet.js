5f04afc09c3f8e15e56784209d3adba3c1891904
Zach <dzacharymason@gmail.com>
2023-08-16 03:26:06 -0500

    use js animation api for moving letters

--- learn-koine/src/Alphabet.js
+++ learn-koine/src/Alphabet.js
@@ -7,6 +7,8 @@ function Letter({ fieldWidth, animationStyles }) {
   const [randomTime, setRandomTime] = useState("");
   const [randomStartPoint, setRandomStartPoint] = useState("");
 
+  const letterRef = useRef();
+
   const alphabetArray = "ασβμ";
   function generateRandomTime() {
     let newTime = Math.random() * 3000 + 2000;
@@ -24,39 +26,80 @@ function Letter({ fieldWidth, animationStyles }) {
     return randomPoint;
   }
 
+  function makeRandomEndPoint(width) {
+    let randomPoint = width - Math.floor(Math.random() * width);
+    let coinToss = Math.random() < 0.5;
+    if (coinToss) {
+      return randomPoint;
+    } else {
+      return "-" + randomPoint;
+    }
+  }
+  // const [randomEndPoint, setRandomEndPoint] = useState("");
+
+  /*
+  @keyframes move {
+  0% {
+    bottom: -55px;
+  }
+  100% {
+    left: var(--random-end);
+    bottom: 700px;
+  }
+  */
+
+  const moving = [
+    { bottom: "-55px", left: randomStartPoint + "px" },
+    { bottom: "700px", left: "0px" },
+  ];
+
+  const movingTiming = {
+    duration: randomTime,
+    iterations: Infinity,
+  };
+
+  const coloring = [
+    { color: "lightblue" },
+    { color: "cyan" },
+    { color: "lightblue" },
+    { color: "cyan" },
+  ];
+
+  const coloringTiming = {
+    duration: 3000,
+    iterations: Infinity,
+  };
+
+  if (fieldWidth && letterRef) {
+    let newEndPoint = makeRandomEndPoint(fieldWidth);
+    let newStartPoint = makeRandomStartPoint();
+    console.log("field width from Letter: " + newEndPoint);
+    moving[0].left = newStartPoint + "px";
+    moving[1].left = newEndPoint + "px";
+    letterRef.current.animate(moving, movingTiming);
+    letterRef.current.animate(coloring, coloringTiming);
+    console.log("start: " + newStartPoint, "stop: " + newEndPoint);
+  }
+
   // const [coloring, coloringTiming] = animationStyles;
 
   useEffect(() => {
-    const coloring = [
-      { color: "lightblue" },
-      { color: "cyan" },
-      { color: "lightblue" },
-      { color: "cyan" },
-    ];
-
-    const coloringTiming = {
-      duration: 3000,
-      iterations: Infinity,
-    };
     let firstLetter = makeRandomLetter();
     let firstTime = generateRandomTime();
-    let startPoint = makeRandomStartPoint();
+    // let startPoint = makeRandomStartPoint();
+    // let endPoint = makeRandomEndPoint(fieldWidth);
     setRandomLetter(firstLetter);
     setRandomTime(firstTime);
-    setRandomStartPoint(startPoint);
-
-    const matches = document.querySelectorAll(".letter");
-    matches.forEach((element) => {
-      element.animate(coloring, coloringTiming);
-    });
-  }, []);
+    // setRandomStartPoint(startPoint);
+    // setRandomEndPoint(endPoint);
+  }, [fieldWidth]);
 
   const handleClick = (e) => {
     console.log(e.target.getAnimations());
     e.preventDefault();
     if (e.target.innerHTML === "α" || e.target.innerHTML === "β") {
-      e.target.style.animationPlayState = "paused";
-      // e.target.animate(coloring, coloringTiming).pause();
+      // e.target.style.animationPlayState = "paused";
+      e.target.animate(moving, movingTiming).pause();
       e.target.style.color = "greenyellow";
       return;
     } else {
@@ -77,26 +120,15 @@ function Letter({ fieldWidth, animationStyles }) {
     }, 500);
   };
 
-  const animationStyle = {
-    animationName: "move",
-    animationDuration: randomTime + "ms",
-    animationTimingFunction: "linear",
-    animationDelay: "0s",
-    animationIterationCount: "infinite",
-    animationFillMode: "forwards",
-    left: randomStartPoint + "%",
-  };
-
-  /*
-  @keyframes move {
-  0% {
-    bottom: -55px;
-  }
-  100% {
-    left: var(--random-end);
-    bottom: 700px;
-  }
-  */
+  // const animationStyle = {
+  //   animationName: "move",
+  //   animationDuration: randomTime + "ms",
+  //   animationTimingFunction: "linear",
+  //   animationDelay: "0s",
+  //   animationIterationCount: "infinite",
+  //   animationFillMode: "forwards",
+  //   left: randomStartPoint + "%",
+  // };
 
   // const flyingLetter = [{ bottom: "-55px" }, { bottom: "700px" }];
 
@@ -108,9 +140,10 @@ function Letter({ fieldWidth, animationStyles }) {
   return (
     <h1
       className="letter"
-      style={animationStyle}
+      // style={animationStyle}
       onClick={(e) => handleClick(e)}
       onAnimationIteration={(e) => handleAnimationIteration(e)}
+      ref={letterRef}
     >
       {randomLetter}
     </h1>
@@ -118,74 +151,43 @@ function Letter({ fieldWidth, animationStyles }) {
 }
 
 const Alphabet = () => {
-  const fieldWidth = useRef();
+  const fieldRef = useRef();
+  const [fieldWidth, setFieldWidth] = useState("");
+
+  // let
+  useEffect(() => {
+    console.log(fieldRef);
+    setFieldWidth(fieldRef.current.getBoundingClientRect().width);
+    console.log(fieldWidth);
+  }, [fieldWidth]);
 
   const checkWidth = () => {
-    console.log(fieldWidth.current.getBoundingClientRect().width);
+    console.log(fieldRef.current.getBoundingClientRect().width);
   };
-
-  // const animationStyle = {
-  //   animationName: "move",
-  //   animationDuration: randomTime + "ms",
-  //   animationTimingFunction: "linear",
-  //   animationDelay: "0s",
-  //   animationIterationCount: "infinite",
-  //   animationFillMode: "forwards",
-  //   left: randomStartPoint + "%",
-  // };
-
-  // const coloring = [
-  //   { color: "lightblue" },
-  //   { color: "cyan" },
-  //   { color: "lightblue" },
-  //   { color: "cyan" },
-  // ];
-
-  // const coloringTiming = {
-  //   duration: 3000,
-  //   iterations: Infinity,
-  // };
-
-  // useEffect(() => {
-  //   const matches = document.querySelectorAll(".letter");
-  //   matches.forEach((element) => {
-  //     element.animate(coloring, coloringTiming);
-  //   });
-  // });
 
   return (
     <>
       <h1>Alphabet Practice</h1>
-      <div className="letter-container" ref={fieldWidth} onClick={checkWidth}>
-        <div className="letter-lane">
+      <div className="letter-container" ref={fieldRef}>
+        <div className="letter-lane" onClick={checkWidth}>
           <Letter
             // animationStyles={[coloring, coloringTiming]}
             fieldWidth={fieldWidth}
+            key="fist"
           />
         </div>
-        <div className="letter-lane">
+        <div className="letter-lane" onClick={checkWidth}>
           <Letter
-          // animationStyles={[coloring, coloringTiming]}
+            // animationStyles={[coloring, coloringTiming]}
+            fieldWidth={fieldWidth}
+            key="second"
           />
         </div>
-        <div className="letter-lane">
+        <div className="letter-lane" onClick={checkWidth}>
           <Letter
-          // animationStyles={[coloring, coloringTiming]}
-          />
-        </div>
-        <div className="letter-lane">
-          <Letter
-          // animationStyles={[coloring, coloringTiming]}
-          />
-        </div>
-        <div className="letter-lane">
-          <Letter
-          // animationStyles={[coloring, coloringTiming]}
-          />
-        </div>
-        <div className="letter-lane">
-          <Letter
-          // animationStyles={[coloring, coloringTiming]}
+            // animationStyles={[coloring, coloringTiming]}
+            fieldWidth={fieldWidth}
+            key="third"
           />
         </div>
       </div>
