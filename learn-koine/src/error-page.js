2bc00c7c06d23d09f8c0b992fd89984e427b88e5
Zach <dzacharymason@gmail.com>
2023-10-17 03:24:07 -0500

    add link to home page for error page

--- learn-koine/src/error-page.js
+++ learn-koine/src/error-page.js
@@ -1,16 +1,20 @@
 import { useRouteError } from "react-router";
+import { Link } from "react-router-dom";
 
-export default function ErrorPage(){
-    const error = useRouteError();
-    console.log(error)
+export default function ErrorPage() {
+  const error = useRouteError();
+  console.log(error);
 
-    return (
-        <div id="error-page">
-        <h1>Oops!</h1>
-        <p>Sorry, an unexpected error has occurred.</p>
-        <p>
-          <i>{error.statusText || error.message}</i>
-        </p>
-      </div>
-    )
+  return (
+    <div id="error-page">
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>{error.statusText || error.message}</i>
+      </p>
+      <u>
+        <Link to="/">Back to Home</Link>
+      </u>
+    </div>
+  );
 }
