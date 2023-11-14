0878faa27cf968236b1a2cfb81fc46f09ab66609
Zach <dzacharymason@gmail.com>
2023-11-14 01:27:39 -0600

    put main game into scene

--- learn-koine/src/games/Starfinder/Alphabet3.js
+++ learn-koine/src/games/Starfinder/Alphabet3.js
@@ -60,31 +60,6 @@ const Alphabet = () => {
     ////////--PHASER SETUP--///////////////////
     ///////////////////////////////////////////
 
-    const config = {
-      type: Phaser.AUTO,
-
-      width: fieldWidth * 0.75,
-      height: fieldHeight * 0.75,
-      parent: "game-field",
-
-      scene: {
-        preload: preload,
-        create: create,
-        update: update,
-      },
-    };
-
-    const game = new Phaser.Game(config);
-    const gameState = {
-      shortArray: firstArray,
-      letterIndex: 0,
-      targetLetter: "α",
-      score: 0,
-      xIncline: config.width < 530 ? 0.2 : 1,
-      xOffset: 7,
-      yOffset: config.width < 530 ? 15 : 18,
-    };
-
     function handleClick(target, tween) {
       if (target.speed === 0 || gameState.score >= 5) {
         return;
@@ -136,243 +111,275 @@ const Alphabet = () => {
       }
     }
 
-    function preload() {
-      this.load.image("logo", "./favicon-32x32.png");
-    }
-
-    function create() {
-      this.cameras.main.setBackgroundColor("#191c24");
-      // this.add.sprite(20, 20, "logo");
-      let dataBox = this.add.rectangle(
-        config.width / 2,
-        config.height / 1.045,
-        config.width + 18,
-        config.height / 11,
-        0x121222
-      );
-
-      dataBox.depth = 1;
-      dataBox.isStroked = true;
-      dataBox.strokeColor = 0x00ffff;
-
-      let scoreFontSize = "24px";
-      gameState.scoreDescription = `Find: ${
-        letterNames[gameState.letterIndex]
-      } - Found: ${gameState.score}`;
-      if (config.width < 415) {
-        scoreFontSize = Math.round(config.width / 18) + "px";
-        gameState.scoreDescription = `Find: ${
-          letterNames[gameState.letterIndex]
-        }\nFound: ${gameState.score}`;
+    class Starfinder extends Phaser.Scene {
+      constructor() {
+        super({ key: "Starfinder" });
       }
 
-      gameState.currentScore = this.add.text(
-        config.width / 25,
-        config.height / 1.085,
-        gameState.scoreDescription,
-        {
-          fontSize: scoreFontSize,
-        }
-      );
+      preload() {
+        this.load.image("logo", "./favicon-32x32.png");
+      }
 
-      gameState.currentScore.depth = 2;
-
-      for (let i = 0; i < numLetters; i++) {
-        let fontSize = "32px";
-        if (config.width < 530) {
-          fontSize = "24px";
-        }
-
-        let startX = Phaser.Math.Between(0, config.width);
-        let startY = Phaser.Math.Between(600, 1000);
-
-        gameState["background" + i] = this.add.rectangle(
-          startX + gameState.xOffset,
-          startY + gameState.yOffset,
-          40,
-          40,
-          "#191c24"
+      create() {
+        this.cameras.main.setBackgroundColor("#191c24");
+        // this.add.sprite(20, 20, "logo");
+        let dataBox = this.add.rectangle(
+          gameState.width / 2,
+          gameState.height / 1.39,
+          gameState.width + 18,
+          gameState.height / 11,
+          0x121222
         );
 
-        gameState["background" + i].setAlpha(0.01);
-        gameState["background" + i].name = "background" + i;
+        dataBox.depth = 1;
+        dataBox.isStroked = true;
+        dataBox.strokeColor = 0x00ffff;
 
-        backgrounds.push(gameState["background" + i]);
+        let scoreFontSize = "24px";
+        gameState.scoreDescription = `Find: ${
+          letterNames[gameState.letterIndex]
+        } - Found: ${gameState.score}`;
+        if (config.width < 415) {
+          scoreFontSize = Math.ceil(config.width / 18) + "px";
+          gameState.scoreDescription = `Find: ${
+            letterNames[gameState.letterIndex]
+          }\nFound: ${gameState.score}`;
+        }
 
-        gameState["letter" + i] = this.add.text(
-          startX,
-          startY,
-          pickRandomLetter(gameState.shortArray),
+        gameState.currentScore = this.add.text(
+          config.width / 25,
+          config.height / 1.085,
+          gameState.scoreDescription,
           {
-            fontFamily: "Helvettica Neue, Times, Courier New, serif",
-            fontSize: fontSize,
-            fill: "#fff",
+            fontSize: scoreFontSize,
           }
         );
 
-        gameState["letter" + i].speed = generateRandomSpeed(config);
-        gameState["letter" + i].direction = Math.random() < 0.5;
-        gameState["letter" + i].name = "letter" + i;
+        gameState.currentScore.depth = 2;
 
-        const primaryColor = Phaser.Display.Color.ValueToColor(0xffffff);
-        const secondaryColor = Phaser.Display.Color.ValueToColor(0x00ffff);
+        for (let i = 0; i < numLetters; i++) {
+          let fontSize = "32px";
+          if (config.width < 530) {
+            fontSize = "24px";
+          }
 
-        const randomDuration = Phaser.Math.Between(300, 2000);
+          let startX = Phaser.Math.Between(0, config.width);
+          let startY = Phaser.Math.Between(600, 1000);
 
-        const letterTween = this.tweens.addCounter({
-          from: 0,
-          to: 100,
-          duration: randomDuration,
-          easing: "Linear",
-          yoyo: true,
-          repeat: -1,
-          onUpdate: (tween) => {
-            const value = tween.getValue();
-            const colorObject = Phaser.Display.Color.Interpolate.ColorWithColor(
-              primaryColor,
-              secondaryColor,
-              100,
-              value
-            );
-
-            const color = Phaser.Display.Color.GetColor(
-              colorObject.r,
-              colorObject.b,
-              colorObject.g
-            );
-
-            gameState["letter" + i].setTint(color);
-          },
-        });
-
-        gameState["letter" + i]
-          .setInteractive()
-          .on("pointerdown", () =>
-            handleClick(gameState["letter" + i], letterTween)
+          gameState["background" + i] = this.add.rectangle(
+            startX + gameState.xOffset,
+            startY + gameState.yOffset,
+            40,
+            40,
+            "#191c24"
           );
 
-        gameState["background" + i]
-          .setInteractive()
-          .on("pointerdown", () =>
-            handleClick(gameState["letter" + i], letterTween)
+          gameState["background" + i].setAlpha(0.01);
+          gameState["background" + i].name = "background" + i;
+
+          backgrounds.push(gameState["background" + i]);
+
+          gameState["letter" + i] = this.add.text(
+            startX,
+            startY,
+            pickRandomLetter(gameState.shortArray),
+            {
+              fontFamily: "Helvettica Neue, Times, Courier New, serif",
+              fontSize: fontSize,
+              fill: "#fff",
+            }
           );
 
-        letters.push(gameState["letter" + i]);
-      }
+          gameState["letter" + i].speed = generateRandomSpeed(config);
+          gameState["letter" + i].direction = Math.random() < 0.5;
+          gameState["letter" + i].name = "letter" + i;
 
-      function randPos() {
-        let x = Phaser.Math.Between(5, config.width);
-        let y = Phaser.Math.Between(5, 550);
-        return { x, y };
-      }
+          const primaryColor = Phaser.Display.Color.ValueToColor(0xffffff);
+          const secondaryColor = Phaser.Display.Color.ValueToColor(0x00ffff);
 
-      function randSize() {
-        let x = Phaser.Math.Between(1, 2);
-        let y = Phaser.Math.Between(1, 2);
-        return { x, y };
-      }
+          const randomDuration = Phaser.Math.Between(300, 2000);
 
-      for (let i = 0; i < numStars; i++) {
-        let pos = randPos();
-        let size = randSize();
+          const letterTween = this.tweens.addCounter({
+            from: 0,
+            to: 100,
+            duration: randomDuration,
+            easing: "Linear",
+            yoyo: true,
+            repeat: -1,
+            onUpdate: (tween) => {
+              const value = tween.getValue();
+              const colorObject =
+                Phaser.Display.Color.Interpolate.ColorWithColor(
+                  primaryColor,
+                  secondaryColor,
+                  100,
+                  value
+                );
 
-        const paleColor = Phaser.Display.Color.ValueToColor(0xffffff);
-        const blueColor = Phaser.Display.Color.ValueToColor(0x00ffff);
+              const color = Phaser.Display.Color.GetColor(
+                colorObject.r,
+                colorObject.b,
+                colorObject.g
+              );
 
-        const randDuration = Phaser.Math.Between(300, 1000);
+              gameState["letter" + i].setTint(color);
+            },
+          });
 
-        gameState["star" + i] = this.add.rectangle(
-          pos.x,
-          pos.y,
-          size.x,
-          size.y,
-          0xffffff
-        );
-
-        this.tweens.addCounter({
-          from: 0,
-          to: 100,
-          duration: randDuration,
-          easing: "Linear",
-          yoyo: true,
-          repeat: -1,
-          onUpdate: (tween) => {
-            const value = tween.getValue();
-            const colorObject = Phaser.Display.Color.Interpolate.ColorWithColor(
-              paleColor,
-              blueColor,
-              100,
-              value
+          gameState["letter" + i]
+            .setInteractive()
+            .on("pointerdown", () =>
+              handleClick(gameState["letter" + i], letterTween)
             );
 
-            const color = Phaser.Display.Color.GetColor(
-              colorObject.r,
-              colorObject.b,
-              colorObject.g
+          gameState["background" + i]
+            .setInteractive()
+            .on("pointerdown", () =>
+              handleClick(gameState["letter" + i], letterTween)
             );
 
-            gameState["star" + i].setFillStyle(color, 50);
-          },
-        });
+          letters.push(gameState["letter" + i]);
+        }
+
+        function randPos() {
+          let x = Phaser.Math.Between(5, config.width);
+          let y = Phaser.Math.Between(5, 550);
+          return { x, y };
+        }
+
+        function randSize() {
+          let x = Phaser.Math.Between(1, 2);
+          let y = Phaser.Math.Between(1, 2);
+          return { x, y };
+        }
+
+        for (let i = 0; i < numStars; i++) {
+          let pos = randPos();
+          let size = randSize();
+
+          const paleColor = Phaser.Display.Color.ValueToColor(0xffffff);
+          const blueColor = Phaser.Display.Color.ValueToColor(0x00ffff);
+
+          const randDuration = Phaser.Math.Between(300, 1000);
+
+          gameState["star" + i] = this.add.rectangle(
+            pos.x,
+            pos.y,
+            size.x,
+            size.y,
+            0xffffff
+          );
+
+          this.tweens.addCounter({
+            from: 0,
+            to: 100,
+            duration: randDuration,
+            easing: "Linear",
+            yoyo: true,
+            repeat: -1,
+            onUpdate: (tween) => {
+              const value = tween.getValue();
+              const colorObject =
+                Phaser.Display.Color.Interpolate.ColorWithColor(
+                  paleColor,
+                  blueColor,
+                  100,
+                  value
+                );
+
+              const color = Phaser.Display.Color.GetColor(
+                colorObject.r,
+                colorObject.b,
+                colorObject.g
+              );
+
+              gameState["star" + i].setFillStyle(color, 50);
+            },
+          });
+        }
       }
-    }
 
-    function update() {
-      gameState.scoreDescription = `Find: ${
-        letterNames[gameState.letterIndex]
-      } - Found: ${gameState.score}`;
-
-      if (config.width < 415) {
+      update() {
         gameState.scoreDescription = `Find: ${
           letterNames[gameState.letterIndex]
-        }\nFound: ${gameState.score}`;
-      }
+        } - Found: ${gameState.score}`;
 
-      gameState.currentScore.text = gameState.scoreDescription;
+        if (config.width < 415) {
+          gameState.scoreDescription = `Find: ${
+            letterNames[gameState.letterIndex]
+          }\nFound: ${gameState.score}`;
+        }
 
-      for (let i = 0; i < letters.length; i++) {
-        let letter = letters[i];
-        let background = backgrounds[i];
-        if (letter.y > -50) {
-          letter.y -= letter.speed;
-          background.y -= letter.speed;
-          if (letter.speed > 0) {
-            if (letter.direction) {
-              letter.x = letter.x + gameState.xIncline;
-              background.x = background.x + gameState.xIncline;
+        gameState.currentScore.text = gameState.scoreDescription;
+
+        for (let i = 0; i < letters.length; i++) {
+          let letter = letters[i];
+          let background = backgrounds[i];
+          if (letter.y > -50) {
+            letter.y -= letter.speed;
+            background.y -= letter.speed;
+            if (letter.speed > 0) {
+              if (letter.direction) {
+                letter.x = letter.x + gameState.xIncline;
+                background.x = background.x + gameState.xIncline;
+              } else {
+                letter.x = letter.x - gameState.xIncline;
+                background.x = background.x - gameState.xIncline;
+              }
             } else {
-              letter.x = letter.x - gameState.xIncline;
-              background.x = background.x - gameState.xIncline;
+              if (gameState.score >= 5) {
+                setTimeout(() => {
+                  letter.speed = config.width < 530 ? 5 : 7;
+                }, 1000);
+              }
             }
           } else {
-            if (gameState.score >= 5) {
-              setTimeout(() => {
-                letter.speed = config.width < 530 ? 5 : 7;
-              }, 1000);
-            }
+            letter.text = pickRandomLetter(gameState.shortArray);
+            letter.speed = generateRandomSpeed(config);
+            background.x =
+              (letter.x = Phaser.Math.Between(0, config.width)) +
+              gameState.xOffset;
+            background.y = (letter.y = 600) + gameState.yOffset;
           }
-        } else {
-          letter.text = pickRandomLetter(gameState.shortArray);
-          letter.speed = generateRandomSpeed(config);
-          background.x =
-            (letter.x = Phaser.Math.Between(0, config.width)) +
-            gameState.xOffset;
-          background.y = (letter.y = 600) + gameState.yOffset;
-        }
 
-        //updating the game state
-        if (gameState.score >= 5) {
-          setTimeout(() => {
-            gameState.score = 0;
-            this.tweens.each((tween) => {
-              if (tween.paused) {
-                tween.restart();
-              }
-            });
-          }, 1000);
+          //updating the game state
+          if (gameState.score >= 5) {
+            setTimeout(() => {
+              gameState.score = 0;
+              this.tweens.each((tween) => {
+                if (tween.paused) {
+                  tween.restart();
+                }
+              });
+            }, 1000);
+          }
         }
       }
     }
+
+    const config = {
+      type: Phaser.AUTO,
+
+      width: fieldWidth * 0.75,
+      height: fieldHeight * 0.75,
+      parent: "game-field",
+
+      scene: [Starfinder],
+    };
+
+    const gameState = {
+      width: fieldWidth,
+      height: fieldHeight,
+      shortArray: firstArray,
+      letterIndex: 0,
+      targetLetter: "α",
+      score: 0,
+      xIncline: config.width < 530 ? 0.2 : 1,
+      xOffset: 7,
+      yOffset: config.width < 530 ? 15 : 18,
+    };
+
+    const game = new Phaser.Game(config);
 
     return () => {
       game.destroy(true);
