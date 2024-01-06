e400b24dfa2f32ab9648f34b93dc042254ef4ff1
DZM <dzacharymason@gmail.com>
2024-01-05 23:15:33 -0600

    begin scoring

---
+++ learn-koine/src/utils.js
@@ -0,0 +1,22 @@
+/*
+    Nouns = 3
+    Ajectives = 3
+    Relative Pronoun = 3
+    Demonstrative pronoun = 3
+    reCiprocal pronoun = 3
+    Personal pronoun = 3
+    correlative pronoun = 3
+    Adverb = 1
+    particle = 1
+    definite article = 3
+    conjunction = 
+    verb = 
+    participle = 
+    infinitive = 
+    preposition =
+*/
+
+//input is an array of strings
+export function scoreVerse(arrayStrings, lexicon) {
+  for (let i = 0; i < arrayStrings.length; i++) {}
+}
